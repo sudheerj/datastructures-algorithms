5b58991d643cf1d350b2cecd800b28d73f7c8579
sudheerj <jsudheer50@gmail.com>
2020-08-06 01:43:09 +0530

    Restructure project

---
+++ src/java/sorting/bubble-sort/bubbleSort.java
@@ -0,0 +1,38 @@
+class BubbleSort
+{
+    /* Swap an array */
+    void swap(int arr[], int firstIndex, int secondIndex) {
+         int temp = arr[firstIndex];
+         arr[firstIndex] = arr[secondIndex];
+         arr[secondIndex] = temp;
+    }
+
+    // Logic of bubble sort
+    void bubbleSort(int arr[]) {
+        int len = arr.length;
+        for (int i = 0; i < len-1; i++)
+            for (int j = 0; j < len-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    swap(arr, j, j+1);
+
+                }
+    }
+
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        System.out.println("Sorted array");
+        for (int i=0; i<arr.length;; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        BubbleSort obj = new BubbleSort();
+        int arr[] = {40, 50, 20, 0, -10, 30, 10};
+        obj.bubbleSort(arr);
+        ob.printArray(arr);
+    }
+}
