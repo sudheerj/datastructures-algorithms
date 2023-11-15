d0ed435ad34740d2856443e73ce41bb5afeedec1
Sudheer Jonna <jsudheer50@gmail.com>
2023-11-15 23:12:05 +0800

    Add BST implementation

--- src/javascript/datastructures/trees/binary_search_tree.js
+++ src/javascript/datastructures/trees/binary_search_tree.js
@@ -15,19 +15,20 @@ class BinarySearchTree {
         const newNode = new Node(value);
         if(this.root === null) {
             this.root = newNode;
-        } else {
+            return this;
+        } 
             const currentNode = this.root;
             while(true) {
                 if(value < currentNode.value) {
                     // Left
-                    if(!currentNode.left) {
+                    if(currentNode.left ===  null) {
                         currentNode.left = newNode;
                         return this;
                     }
                     currentNode = currentNode.left
                 } else if (value > currentNode.right){
                     // Right
-                    if(!currentNode.right) {
+                    if(currentNode.right === null) {
                         currentNode.right = newNode;
                         return this;
                     }
@@ -36,7 +37,6 @@ class BinarySearchTree {
                     return this;
                 }
             }
-        }
         return this;
     }
     lookup(value) {
@@ -55,12 +55,45 @@ class BinarySearchTree {
         }
         return false;
     }
+
+    contains(value) {
+        if (this.root === null) return false;
+        let temp = this.root;
+        while (temp) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    minValueNode(currentNode) {
+        while(true) {
+            if(!currentNode.left) return currentNode;
+            currentNode = currentNode.left;
+        }
+    }
+
+    maxValueNode(currentNode) {
+        while(true) {
+            if(!currentNode.right) return currentNode;
+            currentNode = currentNode.right;
+        }
+    }
 }
 
 const tree = new BinarySearchTree();
 tree.insert(80);
 tree.insert(100);
 tree.insert(50);
-tree.lookup(50);
+console.log(tree.lookup(50));
 tree.insert(90);
-tree.lookup(88);
+console.log(tree.lookup(88));
+console.log(tree.contains(100));
+console.log(tree.contains(9));
+console.log(tree.minValueNode(tree.root.left));
+console.log(tree.maxValueNode(tree.root.right));
