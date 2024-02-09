65ec43f546c21365aca58f07c6b41339035366fa
Sudheer Jonna <jsudheer50@gmail.com>
2024-02-09 17:38:57 +0800

    Add max sum subarray solution in java

---
+++ src/java1/algorithms/array/MaxSumSubArray.java
@@ -0,0 +1,23 @@
+public class MaxSumSubArray {
+    private static int maxSumSubArray(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int currentMaxSum = nums[0], globalMaxSum = nums[0];
+        for(int i=1; i< nums.length; i++) {
+            currentMaxSum = Math.max(nums[i], currentMaxSum+nums[i]);
+            globalMaxSum = Math.max(currentMaxSum, globalMaxSum);
+        }
+
+        return globalMaxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {-7, -2, 3, 4, -5, 6, 7, -2};
+        int maxSumNumbers = maxSumSubArray(numbers);
+        System.out.println("Max Subarray Sum:"+ maxSumNumbers); 
+
+        int[] emptyNumbers = {};
+        int maxSumEmptyNumbers = maxSumSubArray(emptyNumbers);
+        System.out.println("Max Subarray Sum:"+ maxSumEmptyNumbers); 
+    }
+}
