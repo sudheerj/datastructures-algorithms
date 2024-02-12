d8df121e9b1c2d0b7b8374e43abfe7fc7326fe61
Sudheer Jonna <jsudheer50@gmail.com>
2024-02-13 07:01:28 +0800

    Add search rotated sorted array solution

--- src/java1/algorithms/array/SearchRotatedSortedArray.java
+++ src/java1/algorithms/array/SearchRotatedSortedArray.java
@@ -1,3 +1,4 @@
+// TC: O(log n), SC: O(1)
 public class SearchRotatedSortedArray {
     private static int searchRotatedSortedArray(int[] nums, int target) {
         int left = 0, right = nums.length -1;
@@ -7,13 +8,13 @@ public class SearchRotatedSortedArray {
             if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[left] <=nums[mid]) {
+            if(nums[left] <=nums[mid]) { // Left to mid is sorted
                 if(nums[left] <= target && target < nums[mid]) {
                     right = mid-1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
+            } else { // Mid to right is sorted
                 if(nums[mid] < target && target <= nums[right]) {
                     left = mid+1;
                 } else {
--- src/javascript/algorithms/array/searchRotatedSortedArray.js
+++ src/javascript/algorithms/array/searchRotatedSortedArray.js
@@ -1,20 +1,21 @@
+// TC: O(log n), SC: O(1)
 function searchRotatedSortedArray(nums, target) {
     let left = 0;
     let right = nums.length - 1;
 
     while(left <= right) {
-        let mid = left + (right-left)/2;
+        let mid = Math.floor(left + (right-left)/2);
         if(nums[mid] === target) {
             return mid;
         }
 
-        if(nums[left] <= nums[mid]) {
+        if(nums[left] <= nums[mid]) { // Left to mid is sorted
             if(nums[left] <= target && target < nums[mid]) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
-        } else {
+        } else { // Mid to right is sorted
             if(nums[mid] < target && target <= nums[right]) {
                 left = mid + 1;
             } else {
