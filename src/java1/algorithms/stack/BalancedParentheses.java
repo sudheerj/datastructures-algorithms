9baf8c87ee14f1c1c34819b8a77c4f22ca99dfa1
Sudheer Jonna <jsudheer50@gmail.com>
2024-02-18 17:08:33 +0800

    Add balanced parentheses solution

---
+++ src/java1/algorithms/stack/BalancedParentheses.java
@@ -0,0 +1,38 @@
+//Hashmap:- TC:O(n) SC:O(n)
+package java1.algorithms.stack;
+
+import java.util.*;
+
+public class BalancedParentheses {
+    private static boolean hasBalancedParentheses(String str) {
+        if(str.length() == 1 || str.length()%2 !=0) return false;
+        
+        Map<Character, Character> hashMap = new HashMap<>();
+
+        hashMap.put(')', '(');
+        hashMap.put('}', '{');
+        hashMap.put(']', '[');
+
+        Stack<Character> myStack = new Stack<>();
+        for(int i=0; i< str.length(); i++) {
+            char ch = str.charAt(i);
+            if(hashMap.containsKey(ch)) {
+                if(myStack.size() != 0 && myStack.pop() != hashMap.get(ch)) {
+                    return false;
+                }
+            } else {
+                myStack.push(ch);
+            }
+        }
+        return myStack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str1 = "{([])}";
+        System.out.println(hasBalancedParentheses(str1));
+
+        String str2 = "{(][)}";
+        System.out.println(hasBalancedParentheses(str2));
+    }
+
+}
