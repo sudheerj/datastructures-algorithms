9dbd0fbfecd84a397730bf5cc804c4b48fa87788
Sudheer Jonna <jsudheer50@gmail.com>
2024-09-04 00:02:32 +0800

    Implement lodash get function

--- src/javascript/algorithms/misc/lodash/get/get.js
+++ src/javascript/algorithms/misc/lodash/get/get.js
@@ -1,3 +1,18 @@
-function get() {
-    
+function get(obj, path, defaultVal) {
+    let index = 0;
+    let pathArr = Array.isArray(path) ? path : path.split('.');
+    const length = pathArr.length;
+
+    while(obj != null && index < length) {
+        obj = obj[String(pathArr[index])];
+        index++;
+    }
+
+    return index === length ? obj : defaultVal;
 }
+
+let object1 = { 'a': { 'b': { 'c': 3 } } };
+let object2 = { 'a': [{ 'b': { 'c': 3 } }] };
+console.log(get(object1, 'a.b.c'));
+console.log(get(object2, ['a', '0', 'b', 'c']));
+console.log(get(object2, 'a.b.c', 'default'));
